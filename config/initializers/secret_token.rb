6ab505874c6da401a1c288f118721bc8e590aad8
tkawa <tkawa@4bit.net>
2012-04-28 23:45:14 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeepExpRecord::Application.config.secret_token = '9d7f7933ca4be4af3427159144e086a99e86aa9589f6846970818fa78283fd12c0e6d62fb4f5917a902a525c7418700e4c40241d55f7409f7c88698d3bb92139'
